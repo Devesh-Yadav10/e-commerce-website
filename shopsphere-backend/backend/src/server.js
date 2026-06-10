ecd6a5cee47c46cc8f1abac8e8be0f7186c021da
Devesh Yadav <160987201+Devesh-Yadav10@users.noreply.github.com>
2026-06-10 12:42:20 +0530

    fix2

--- shopsphere-backend/backend/src/server.js
+++ shopsphere-backend/backend/src/server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(cors({
   origin: [
     "http://localhost:3000",       // local dev
-    "https://yourapp.vercel.app",  // your actual Vercel URL
+    "https://e-commerce-website-phi-gilt.vercel.app/",  // your actual Vercel URL
   ],
   credentials: true,
 }));
